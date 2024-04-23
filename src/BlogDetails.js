d108b2c0c74ee17da3ffd544e70d52ec36b3a42d
Jinendra <jinendray1008@gmail.com>
2024-04-24 01:03:02 +0530

    last  commit

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,8 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import UseFetch from "./UseFetch";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -7,7 +10,16 @@ const BlogDetails = () => {
     data: blog,
     error,
     isPending,
-  } = UseFetch("http://localhost:8000/blogs/" + id);
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +30,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-  const [title, setTitle] = useState();
-  const [body, setBody] = useState();
-  const [author, setAutor] = useState();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+    // history.go(-1);
+    history.push("/");
+  };
+
   return (
     <div className="create">
-      <h2>Add Blog</h2>
-      <form>
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -15,22 +37,19 @@ const Create = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <label>Blog Body:</label>
+        <label>Blog body:</label>
         <textarea
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAutor(e.target.value)}>
-          <option value="mario">Mario</option>
-          <option value="yoshi">jinu</option>
-          <option value="mario">Mario</option>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
